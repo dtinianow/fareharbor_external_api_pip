0424d28f89fed1c3dced206c37871f0e6935dfab
Zack Forbing <zackforbing@gmail.com>
2016-09-18 17:38:30 -0600

    add print methods to prove class works

--- fareharbor_external_api/availability.py
+++ fareharbor_external_api/availability.py
@@ -6,3 +6,11 @@ class Availability(object):
 a = {'capacity': 10, 'customer_type_rates': [1, 5], 'custom_field_instances': "", 'item': 'nope', 'pk': 6, 'start_at': 'mmm never', 'end_at': 'mmm yesterday'}
 
 avail = Availability(a)
+
+print avail
+print avail.capacity
+print avail.customer_type_rates
+print avail.custom_field_instances
+print avail.item
+print avail.start_at
+print avail.end_at
