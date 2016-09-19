b10bc9d2c700bab5f887aca2fe51730a59c07bfb
David Tinianow <david.tinianow@gmail.com>
2016-09-19 15:19:41 -0600

    Companies class (#7)
    * Create companies class

--- fareharbor/companies.py
+++ fareharbor/companies.py
@@ -1,3 +1,22 @@
-import fare_harbor_service
+from fare_harbor_service import FareHarborService
+import json
 
-class Companies(object):
+class Companies(FareHarborService):
+
+    def all(self):
+        raw_data = FareHarborService().get_companies()
+        return raw_data['companies']
+        # companies = raw_data['companies']
+        # for i in companies:
+        #     print companies[i]
+
+x = Companies()
+x.all()
+
+# class Companies(FareHarborService):
+#
+#     def service(self):
+#         print FareHarborService().get_companies()
+#
+# x = Companies()
+# x.service()
--- fareharbor/fare_harbor_service.py
+++ fareharbor/fare_harbor_service.py
@@ -17,51 +17,51 @@ class FareHarborService:
 
     def get_companies(self):
         response = requests.get('%scompanies/' % self.url, params=self.payload)
-        print response.json()
+        return response.json()
 
     def get_items(self, shortname):
         response = requests.get('%scompanies/%s/items/' % (self.url, shortname), params=self.payload)
-        print response.json()
+        return response.json()
 
     def get_availabilities_by_date(self, shortname, data):
         response = requests.get('%scompanies/%s/items/%s/minimal/availabilities/date/%s/' % (self.url, shortname, data['pk'], data['date']), params=self.payload)
-        print response.json()
+        return response.json()
 
     def get_availabilities_by_date_range(self, shortname, data):
         response = requests.get('%scompanies/%s/items/%s/minimal/availabilities/date-range/%s/%s/' % (self.url, shortname, data['pk'], data['start_date'], data['end_date']), params=self.payload)
-        print response.json()
+        return response.json()
 
     def get_availability(self, shortname, pk):
         response = requests.get('%scompanies/%s/availabilities/%s/' % (self.url, shortname, pk), params=self.payload)
-        print response.json()
+        return response.json()
 
     def get_booking(self, shortname, uuid):
         response = requests.get('%scompanies/%s/bookings/%s/' % (self.url, shortname, uuid), params=self.payload)
-        print response.json()
+        return response.json()
 
     def get_lodgings(self, shortname):
         response = requests.get('%scompanies/%s/lodgings/' % (self.url, shortname), params=self.payload)
-        print response.json()
+        return response.json()
 
     def get_availability_lodgings(self, shortname, pk):
         response = requests.get('%scompanies/%s/availabilities/%s/lodgings/' % (self.url, shortname, pk), params=self.payload)
-        print response.json()
+        return response.json()
 
     def post_booking(self, data):
         raw_booking = self.format_booking_body(data)
         booking = json.dumps(raw_booking)
         response = requests.post('%scompanies/%s/availabilities/%s/bookings/' % (self.url, data['company_shortname'], data['pk']), data=booking, params=self.payload)
-        print response.json()
+        return response.json()
 
     def post_verify_booking(self, data):
         raw_booking = self.format_booking_body(data)
         booking = json.dumps(raw_booking)
         response = requests.post('%scompanies/%s/availabilities/%s/bookings/validate/' % (self.url, data['company_shortname'], data['pk']), data=booking, params=self.payload)
-        print response.json()
+        return response.json()
 
     def delete_booking(self, shortname, uuid):
         response = requests.delete('%scompanies/%s/bookings/%s/' % (self.url, shortname, uuid), params=self.payload)
-        print response.json()
+        return response.json()
 
     def format_booking_body(self, data):
         return {
